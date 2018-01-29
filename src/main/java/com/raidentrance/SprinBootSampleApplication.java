50ec3b3a55db4f32bb1224f1643439a17381f76e
Alex <raidentrance@gmail.com>
2018-01-29 14:46:38 -0600

    include metrics support

--- src/main/java/com/raidentrance/SprinBootSampleApplication.java
+++ src/main/java/com/raidentrance/SprinBootSampleApplication.java
@@ -5,9 +5,13 @@ package com.raidentrance;
 
 import org.jolokia.http.AgentServlet;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jmx.export.MBeanExporter;
 
 /**
  * @author raidentrance
@@ -19,6 +23,12 @@ public class SprinBootSampleApplication {
 	@Bean
 	public ServletRegistrationBean servletRegistrationBean() {
 		return new ServletRegistrationBean(new AgentServlet(), "/jolokia/*");
+	}
+
+	@Bean
+	@ExportMetricWriter
+	public MetricWriter metricWriter(MBeanExporter exporter) {
+		return new JmxMetricWriter(exporter);
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/raidentrance/resource/UserResource.java
+++ src/main/java/com/raidentrance/resource/UserResource.java
@@ -9,8 +9,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
 import org.springframework.stereotype.Component;
 
 /**
@@ -23,12 +23,12 @@ import org.springframework.stereotype.Component;
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
-
-	private static final Logger log = LoggerFactory.getLogger(UserResource.class);
-
+	
+	@Autowired
+	private CounterService counterService;
 	@GET
 	public String getUsers() {
-		log.info("Getting user");
+		counterService.increment("services.system.myservice.invoked");
 		return String.format("{\"username\":\"raidentrance\"}");
 	}
 
